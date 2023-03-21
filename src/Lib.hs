3f205853e4c6034eeb8fcb2037ae45f6734a2557
Mehdi Djendar <mehdi.djendar@epitech.eu>
2023-03-21 12:58:05 +0100

    feat: add init conf struct with flag arg (n l f)

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,12 +3,31 @@ module Lib (my_image_compressor) where
 import System.Environment (getArgs)
 import System.Exit
 
-calculDistance :: [String] -> IO ()
-calculDistance [] = return ()
-calculDistance a b = return ()
+data Param = Param {
+    number :: Int,
+    limit :: Float,
+    file :: String
+} deriving (Eq, Show)
+
+init_param :: [String] -> Param -> Param
+init_param [] param = param
+init_param ("-n":sec:list) (Param _ l f) = init_param list (Param (read sec :: Int ) l f)
+init_param ("-l":sec:list) (Param n _ f) = init_param list (Param n (read sec :: Float ) f)
+init_param ("-f":sec:list) (Param n l _) = init_param list (Param n l (sec))
+init_param (_:_:_) (Param n l f) = (Param n l f)
+
+default_param :: Param
+default_param = Param {number = 0, limit = -1, file = "test.txt"}
+
+display_param :: Param -> IO ()
+display_param param = do
+    putStrLn $ "number: " ++ (show $ number param)
+    putStrLn $ "limit: " ++ (show $ limit param)
+    putStrLn $ "file: " ++ (file param)
 
 my_image_compressor :: IO ()
 my_image_compressor = do
     args <- getArgs
-    calculDistance args
+    let param = init_param args default_param
+    display_param param
     exitSuccess
